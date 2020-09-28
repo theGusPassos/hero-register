fb67460fdd786011fc3559f512f7c08cbb199445
Gustavo Passos <theguspassos@gmail.com>
2020-09-28 15:50:31 -0300

    add dev servlet

---
+++ src/hero_project/components/dev_servlet.clj
@@ -0,0 +1,22 @@
+(ns hero-project.dev-servlet
+  (:require [com.stuartsierra.component :as component]
+            [io.pedestal.http :as bootstrap]))
+
+(defrecord DevServlet [service]
+  component/Lifecycle
+
+  (start [this]
+    (assoc this :instance (-> service
+                              :runnable-service
+                              ;; what does this do?
+                              (assoc ::bootstrap/join? false)
+                              bootstrap/create-server
+                              bootstrap/start)))
+  (stop [this]
+    (bootstrap/stop (:instance this))
+    (dissoc this :instance)))
+
+(defn new-servlet [] (map->DevServlet {}))
+
+(defn main [start-fn & _args]
+  (start-fn {:mode :embedded}))
