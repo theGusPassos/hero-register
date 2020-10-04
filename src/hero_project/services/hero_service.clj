a5d731180db8bdc783cb59873934d645946c5ad1
Gustavo Passos <theguspassos@gmail.com>
2020-10-04 16:15:15 -0300

    set response as json

--- src/hero_project/services/hero_service.clj
+++ src/hero_project/services/hero_service.clj
@@ -1,17 +1,15 @@
 (ns hero-project.services.hero-service
   (:require [ring.util.response :as ring-resp]
             [hero-project.controllers.hero-controller :as controller]
-            [hero-project.adapters.hero-adapter :as adapter]))
-
-(defn home-page
-  [_]
-  (ring-resp/response {:game-title "hero project"}))
+            [hero-project.adapters.hero-adapter :as adapter]
+            [clojure.data.json :as json]))
 
 (defn heroes
   [{{:keys [storage]} :components}]
   (let [heroes (controller/heroes storage)]
     (->>  heroes
           adapter/heroes->hero-view
+          json/write-str
           ring-resp/response)))
 
 (defn create-hero
@@ -20,6 +18,7 @@
   (let [hero-created (controller/create-hero! name storage)]
     (->> hero-created
          adapter/hero->hero-view
+         json/write-str
          ring-resp/response)))
 
 (defn get-hero
@@ -28,6 +27,7 @@
   (if-let [hero (controller/get-hero (adapter/str->uuid hero-id) storage)]
     (->> hero
          adapter/hero->hero-view
+         json/write-str
          ring-resp/response)
     (ring-resp/not-found {})))
 
